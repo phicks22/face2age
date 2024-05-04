7cd4be40bd80b13656be19e6ebbb633c3b05e9ba
Parker Hicks <parkerhicks99@gmail.com>
2024-05-04 01:09:46 -0400

    removed unused packages

--- test.py
+++ test.py
@@ -4,13 +4,10 @@ import pandas as pd
 from tqdm import tqdm
 import torch.nn as nn
 from pathlib import Path
-import torch.optim as optim
-import torchvision.models as models
-from image_dataset import ImageDataset
-from sklearn.model_selection import train_test_split
+from image_dataset import ImageDataset, TestDataset
 from torchvision.transforms import ToPILImage
 from torch.utils.data import DataLoader, Subset
-from network import Model 
+from model import Model 
 from argparse import ArgumentParser
 from loss import RMSLE
 
@@ -35,6 +32,7 @@ if __name__ == '__main__':
     )
     parser.add_argument(
         '--test_set',
+        action='store_true',
     )
     parser.add_argument(
         '-outfile',
@@ -44,24 +42,49 @@ if __name__ == '__main__':
     )
     args = parser.parse_args()   
     start = time.time()
+    
+    # Define variables
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    batch_size = args.batch_size
+    weights_file = Path(args.weights)
 
     if args.test_set:
         TEST_IMGS = DATA_DIR / 'wiki_labeled'
-        ids_file = DATA_DIR / 'wiki_labels.csv'
+        labels_file = DATA_DIR / 'wiki_labels.csv'
+        test_indices_file = DATA_DIR / "test_indices.txt"
+
+        # Load data
+        data = ImageDataset(
+            labels_file, 
+            TEST_IMGS,
+            transform=ToPILImage()
+        )
+        
+        test_indices = list(np.loadtxt(test_indices_file))
+        
+        # Define datasets for each 
+        test_dataset = Subset(data, test_indices)
+        test_loader = DataLoader(test_dataset, batch_size=batch_size, num_workers=1, shuffle=False)
 
     else:
         TEST_IMGS = DATA_DIR / 'wiki_judge_images'
         ids_file = DATA_DIR / 'wiki_judge.csv'
-    
-    # Define variables
-    batch_size = args.batch_size
-    weights_file = Path(args.weights)
-    
+        
+        # Load data
+        data = TestDataset(
+            ids_file, 
+            TEST_IMGS,
+            transform=ToPILImage()
+        )
+        
+        # Define datasets for each 
+        test_loader = DataLoader(data, batch_size=batch_size, num_workers=1, shuffle=False)
+
     # Define model
     weights = torch.load(weights_file)
     model = Model()
     model.to(device)
-    model.load_state_dict(weights) 
+    model.load_state_dict(weights)
     model.eval()   
 
     predictions = {'ID': [], 'age': []} 
@@ -69,13 +92,15 @@ if __name__ == '__main__':
         predict_bar = tqdm(test_loader)
         for data, _id in predict_bar:
             batch_size = data.size(0)
-            valid_result['nsamples'] = batch_size
 
             z = data.to(device)
             pred_age = model(z.float())
-            predictions['age'].append(pred_age.to('cpu').numpy()[0][0])
-            predictions['ID'].append(_id[0])
-    
+            predictions['age'].append(pred_age.item())
+            
+            if args.test_set:
+                predictions['ID'].append(_id[0].item())
+            else:
+                predictions['ID'].append(_id.item())
     # Save predictions
     pred_df = pd.DataFrame.from_dict(predictions)  
     outfile = RESULTS_DIR / args.outfile
